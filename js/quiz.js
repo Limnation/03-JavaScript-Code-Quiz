91f30b72fbfe219f5bcd4023995e38d0f028ade0
Scott Erwin <scottallenerwin@gmail.com>
2021-03-09 17:43:05 -0500

    added new js quetions and populated the questions

---
+++ js/quiz.js
@@ -0,0 +1,71 @@
+//My questions array ref:https://www.w3schools.com/js/js_arrays.asp
+let myQuestions = [
+    {
+        questionNum: 1,
+        question: "Commonly used data type DO NOT include",
+        answer: "3. alerts",
+        options: [
+            "1. strings",
+            "2. booleans",
+            "3. alerts",
+            "4. functions"
+        ]
+    },
+    {
+        questionNum: 2,
+        question: "The condition an if / else statement is enclosed within ______.",
+        answer: "2. curly brackets",
+        options: [
+            "1. quotes",
+            "2. curly brackets",
+            "3. parenthesis",
+            "4. square brackets"
+        ]
+    },
+    {
+        questionNum: 3,
+        question: "Arrays in JavaScript can be used to store",
+        answer: "4. all of the above",
+        options: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above"
+        ]
+    },
+    {
+        questionNum: 4,
+        question: "Strings values be enclosed within ______ when being assinged to variables",
+        answer: "4. parentheses",
+        options: [
+            "1. commas",
+            "2. curly brackets",
+            "3. quotes",
+            "4. parentheses"
+        ]
+    },
+    {
+        questionNum: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "4. console.log",
+        options: [
+            "1. JavaScript",
+            "2. terminal",
+            "3. for loops",
+            "4. console.log"
+        ]
+    },
+]
+
+//Template
+// {
+//     questionNum: 1,
+//     question: "",
+//     answer: "",
+//     options: [
+//         "1. ",
+//         "2. ",
+//         "3. ",
+//         "4. "
+//     ]
+// },
